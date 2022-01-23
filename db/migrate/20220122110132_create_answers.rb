cad39d7864a159edcbd47576d5ec527353ca7361
mvpurin <mvpurin@gmail.com>
2022-01-24 00:11:36 +0700

    Part3 после исправлений

--- db/migrate/20220122110132_create_answers.rb
+++ db/migrate/20220122110132_create_answers.rb
@@ -1,9 +1,10 @@
 class CreateAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :answers do |t|
-      t.string :body
-      t.integer :user_id
-      t.integer :question_id
+      t.string :body, null: false
+      t.references :user, foreign_key: true, null: false
+      t.references :question, foreign_key: true, null: false
+      t.boolean :correct, default: false
 
       t.timestamps
     end
--- db/migrate/20220122151907_add_questions_test_id_null_constraint.rb
+++
@@ -1,5 +0,0 @@
-class AddQuestionsTestIdNullConstraint < ActiveRecord::Migration[6.0]
-  def change
-    change_column_null(:questions, :test_id, false)
-  end
-end
