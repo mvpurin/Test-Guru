375cb19d8094a510db957a4a348940f82ff99a9b
mvpurin <mvpurin@gmail.com>
2022-01-24 20:31:03 +0700

    Part3 выполнена

--- db/migrate/20220122110132_create_answers.rb
+++ db/migrate/20220122110132_create_answers.rb
@@ -2,7 +2,6 @@ class CreateAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :answers do |t|
       t.string :body, null: false
-      t.references :user, foreign_key: true, null: false
       t.references :question, foreign_key: true, null: false
       t.boolean :correct, default: false
 
--- db/schema.rb
+++ db/schema.rb
@@ -14,13 +14,11 @@ ActiveRecord::Schema.define(version: 2022_01_22_155539) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
-    t.integer "user_id", null: false
     t.integer "question_id", null: false
     t.boolean "correct", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
-    t.index ["user_id"], name: "index_answers_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -53,7 +51,6 @@ ActiveRecord::Schema.define(version: 2022_01_22_155539) do
   end
 
   add_foreign_key "answers", "questions"
-  add_foreign_key "answers", "users"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
 end
