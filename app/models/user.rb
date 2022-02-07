42bdddd345a46ce82e0e8fa4c47f180bf6c6a352
mvpurin <mvpurin@gmail.com>
2022-02-08 00:49:10 +0700

    Закончил поиск тестов по уровню в модели User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_many :test_users
   has_many :tests, through: :test_users, dependent: :destroy
+
+  def find_test_by_level(level)
+    tests_id = test_users.where(user_id: self.id).pluck(:test_id)
+    tests.where(id: tests_id, level: level)
+  end
 end
