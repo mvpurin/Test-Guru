9b8c4a0ec90dfcd5763a784a0d4b1b0a8bfd7a5a
mvpurin <mvpurin@gmail.com>
2022-03-14 23:37:29 +0700

    part 7 выполнение

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,28 @@
 class QuestionsController < ApplicationController
   def index
     #byebug
-    @tests = Test.find(params[:id])
+    @questions = Question.where(:test_id => params[:test_id])
+    byebug
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def new
+    
+  end
+
+  def create
+    quest = Question.create(question_params)
+  end
+
+  def destroy
+    Question.find(params[:id]).destroy!
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:body, :test_id)
   end
 end
