e89b633034f818c627e41a1b821ff0f039614ea8
mvpurin <mvpurin@gmail.com>
2022-03-20 22:27:20 +0700

    part 7 после исправления

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,39 +1,54 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: %i[index]
+  before_action :find_test, only: %i[index, new, create]
+  before_action :find_question, only: %i[show, destroy]
 
-  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+  # rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
     find_test
-    render html: @result.questions.map {|question| question.body}
+    render html: @test.questions.map {|question| question.body}
   end
 
   def show
-    @question = Question.find(params[:id])
+    find_question
     render html: @question.body
   end
 
   def new
+    find_test
   end
 
   def create
-    quest = Question.create(question_params)
+    byebug
+    question = @test.questions.new(question_params)
+    if question.save
+      render plain: "Good"
+    else
+      render plain: "Bad"
+    end
   end
 
   def destroy
-    Question.find(params[:id]).destroy!
+    question = find_question
+    question.destroy
+
+    puts "Question was not deleted" if question.exists?
   end
 
   private
   def question_params
-    params.require(:question).permit(:body, :test_id)
+    params.require(:question).permit(:body)
   end
 
   def find_test
-    @result = Test.find(params[:test_id])
+    @test = Test.find(params[:test_id])
   end
 
-  def rescue_with_question_not_found
-    render plain: 'Question not found'
+  def find_question
+    @question = Question.find(params[:id])
   end
+
+  # def rescue_with_question_not_found
+  #   render plain: 'Question not found'
+  # end
 end
