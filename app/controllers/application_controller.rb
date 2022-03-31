d6466e596bd367b618e5ccaad594698266c26efd
mvpurin <mvpurin@gmail.com>
2022-03-31 22:42:53 +0700

    part 8 в процессе выполнения

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,26 +1,27 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: [:index, :new, :create]
+  before_action :find_test, only: [:index, :new, :create, :show]
   before_action :find_question, only: [:show, :destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
-    render html: @test.questions.map {|question| question.body}
   end
 
   def show
-    render html: @question.body
   end
 
   def new
+    @question = @test.questions.new
   end
 
   def create
-    question = @test.questions.new(question_params)
-    if question.save
-      render plain: "Good"
+    @question = @test.questions.new(question_params)
+    byebug
+
+    if @question.save
+      redirect_to @question
     else
-      render plain: "Bad"
+      render :new
     end
   end
 
@@ -35,15 +36,8 @@ class QuestionsController < ApplicationController
     params.require(:question).permit(:body)
   end
 
-  def find_test
-    @test = Test.find(params[:test_id])
-  end
-
-  def find_question
-    @question = Question.find(params[:id])
-  end
-
   def rescue_with_question_not_found
     render plain: 'Question not found'
   end
+
 end
