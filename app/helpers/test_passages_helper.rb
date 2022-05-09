0ff7d1661261d36fcbadd405c3042f565934bc04
mvpurin <mvpurin@gmail.com>
2022-05-09 22:55:55 +0700

    part9 на проверку

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,11 +1,15 @@
 module TestPassagesHelper
 
   def score(test_passage)
-    test_passage.correct_questions / test_passage.test.questions.count
+    test_passage.correct_questions.to_f / test_passage.test.questions.count.to_f
   end
 
   def pass_the_test?(test_passage)
     score(test_passage) >= 0.85
+  end
+
+  def question_counter(test_passage)
+    test_passage.test.questions.ids.index(test_passage.current_question.id) + 1
   end
 
 end
