70f50da761b96f8590235f4de2fb71400a97c1ed
mvpurin <mvpurin@gmail.com>
2022-06-27 23:48:00 +0700

    Part11 на проверку

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
 
+  before_action :hello
+
   def after_sign_in_path_for(resource)
     if current_user.is_a?(Admin)
     '/admin/tests'
   else
     '/tests'
     end
+  end
+
+  def hello
+    flash.now[:alert] = "Hello, #{current_user.first_name} #{current_user.last_name}!" if !current_user.nil?
   end
 
 end
