9a22efae50b8a954cf0b9b067dd496ea98e3ff84
mvpurin <mvpurin@gmail.com>
2022-10-28 01:02:50 +0700

    Добавлена проверка совпадения паролей пользователя При сортировке значений тестов пользователя меняются местами две последние строки

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,7 @@ require("jquery")
 //require("popper")
 //require("bootstrap-sprockets")
 require("utilities/sorting.js")
+require("utilities/password_confirmation.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
