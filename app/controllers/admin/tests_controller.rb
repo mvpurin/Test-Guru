23c21043aab049fb4c9891a5ece4eb1ab87663ad
mvpurin <mvpurin@gmail.com>
2022-11-17 00:16:38 +0700

    Выполнены действия из видео, до применения jQuery

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,9 +1,10 @@
 class Admin::TestsController < Admin::BaseController
 
-  before_action :find_test, only: :destroy
+  before_action :set_tests, only: %i[index update_inline]
+  before_action :find_test, only: %i[destroy update_inline]
 
   def index
-    @tests = Test.all
+
   end
 
   def new
@@ -37,8 +38,19 @@ class Admin::TestsController < Admin::BaseController
     end
   end
 
+  def update_inline
+    if @test.update(test_params)
+      redirect_to admin_tests_path
+    else
+      render :index
+    end
+  end
 
   private
+
+  def set_tests
+    @tests = Test.all
+  end
 
   def test_params
     params.require(:test).permit(:title, :level, :category_id)
