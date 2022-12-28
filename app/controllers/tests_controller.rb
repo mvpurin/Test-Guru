d17ac9081abf22f2a601b3a18650a3ee54af13a1
mvpurin <mvpurin@gmail.com>
2022-12-28 22:02:30 +0700

    1. Мин значение для уровня теста 2. Пользователь не видит пустые тесты

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,7 +1,8 @@
-class TestsController < ApplicationController 
-  
+class TestsController < ApplicationController
+  before_action :set_tests, only: :index
+
   def index
-    @tests = Test.all
+    @tests = test_not_empty?
   end
 
   def start
@@ -9,5 +10,15 @@ class TestsController < ApplicationController
     current_user.tests.push(@test)
     redirect_to current_user.test_passage(@test)
   end
-  
+
+  private
+
+  def set_tests
+    @tests = Test.all
+  end
+
+  def test_not_empty?
+    @tests.select {|test| test.questions.size > 0}
+  end
+
 end
